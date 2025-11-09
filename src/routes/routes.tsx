7c7ad9c9364cc021d8bc8ed675cc926f2fde5170
Devbhuti-Nandika <nandik.bitan@gmail.com>
2025-11-09 22:02:32 +0530

    feat: add initial setup for ui

---
+++ src/routes/routes.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router";
+import type { RouteObject } from "react-router";
+import Login from "@pages/Login";
+import Register from "@pages/Register";
+import Home from "@pages/Home";
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+export default router;
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@routes": path.resolve(__dirname, "./src/routes"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@contexts": path.resolve(__dirname, "./src/contexts"),
+      "@types": path.resolve(__dirname, "./src/types"),
+    },
+  },
+});

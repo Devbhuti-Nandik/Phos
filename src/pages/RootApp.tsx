2b56cea257b9c989dca2c4b392ff7d801020eccd
Devbhuti-Nandika <nandik.bitan@gmail.com>
2026-05-23 12:18:54 +0530

    feat: fix core stylings

--- src/pages/RootApp.tsx
+++ src/pages/RootApp.tsx
@@ -1,40 +1,55 @@
 const RootApp = () => {
   return (
-    <div className="h-screen bg-appBg flex items-center justify-center p-4">
+    <div
+      className="
+      h-screen 
+      w-full 
+      bg-panelBg 
+      bg-linear-to-br
+      from-[#151822]
+      via-appBg
+      to-[#090a0f] flex-1 flex-col overflow-hidden"
+    >
       {/* Main App Container */}
-      <div
-        className=" 
-        w-full h-full 
-        bg-panelBg 
-        rounded-2xl 
-        shadow-[0_20px_60px_rgba(0,0,0,0.6)] 
-        overflow-hidden
-        p-4
-        "
-      >
-        {/* Top Bar */}
-        <div className="h-16 text-white bg-softBg flex items-center px-6 rounded-2xl border-b border-white/5">
-          Toolbar
-        </div>
-
+      {/* Top Bar */}
+      <div className="h-20 text-white flex items-center px-6 border-b border-white/5">
+        Topbar
+      </div>
+      <div>
         {/* Middle Section */}
-        <div className="min-h-[82vh] flex flex-1 overflow-hidden justify-between gap-4">
+        <div className="h-screen w-full flex flex-1 overflow-hidden justify-between gap-4">
           {/* Left Panel */}
-          <div className="min-h-[76vh] w-52 text-white bg-softBg mt-6 p-4 rounded-2xl border-r border-white/5">
+          <div className="h-5/6 w-1/5 text-white glass my-6 mx-4 p-4 rounded-2xl border-r border-white/5">
             Presets
           </div>
-          <div className="mt-6 flex flex-col justify-between gap-4">
+          <div className="h-5/6 w-3/5 mt-6 flex flex-col justify-between gap-4">
             {/* Canvas Area */}
-            <div className="text-white w-5xl bg-softBg flex-1 flex items-center justify-center rounded-2xl">
+            <div
+              className="
+              h-4/5 
+              flex 
+              flex-col 
+              items-center 
+              justify-center 
+              text-white ring-1 
+              ring-white/[0.04]   
+              relative
+              z-10
+              bg-black/40
+              border border-white/10
+              p-6
+              rounded-2xl
+              shadow-[0_20px_80px_rgba(0,0,0,0.5)]"
+            >
               Canvas
             </div>
             {/* Bottom Filmstrip */}
-            <div className="h-24 min-w-4xl text-white bg-softBg flex items-center px-4 gap-3 rounded-2xl border-t border-white/5">
+            <div className="h-1/5 text-white glass flex items-center px-4 gap-3 rounded-2xl border-t border-white/5">
               Thumbnails
             </div>
           </div>
           {/* Right Panel */}
-          <div className="min-h-[76vh] w-72 text-white bg-softBg mt-6 p-4 rounded-2xl border-l border-white/5">
+          <div className="h-5/6 w-1/5 text-white glass my-6 mx-4 p-4 rounded-2xl border-l border-white/5">
             Adjustments
           </div>
         </div>
